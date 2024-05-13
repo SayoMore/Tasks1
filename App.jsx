e88a13ecb9864a7b9880db15243c81d30e1ee7fb
SayoMore <96719695+SayoMore@users.noreply.github.com>
2024-05-14 00:20:45 +0530

    Created App component

---
+++ App.jsx
@@ -0,0 +1,15 @@
+import "./style.css";
+import InputItems from "./components/inputItems";
+
+function App() {
+  return (
+    <>
+      <center>
+        <h1 className="heading">To-Do List</h1>
+        <InputItems />
+      </center>
+    </>
+  );
+}
+
+export default App;
