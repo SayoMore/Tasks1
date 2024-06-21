9ca47971863b2222c20bacf840ba6e73dab37ad7
Sayali More <96719695+SayoMore@users.noreply.github.com>
2024-06-21 19:39:59 +0530

    Create Banner.jsx

---
+++ Banner.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "../styles/Banner.css";
+
+function Banner() {
+  return (
+    <div className="banner">
+      <img
+        src="https://img.freepik.com/freevector/watercolor-emerald-background_23-2150238649.jpg"
+        alt="Banner"
+      />
+    </div>
+  );
+}
+
+export default Banner;
