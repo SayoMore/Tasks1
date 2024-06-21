b3a1bf602e91f07e4947d7b7e49f391cea40d4f8
Sayali More <96719695+SayoMore@users.noreply.github.com>
2024-06-21 19:41:48 +0530

    Create Search.jsx

---
+++ Search.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../styles/SearchBar.css";
+
+function SearchBar() {
+  return (
+    <div className="search-bar">
+      <input type="text"></input>
+    </div>
+  );
+}
+
+export default SearchBar;
