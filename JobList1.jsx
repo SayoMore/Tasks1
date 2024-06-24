87f0ed2c39a828c5d5e816c41a57e51e3e07bb53
Sayali More <96719695+SayoMore@users.noreply.github.com>
2024-06-24 14:11:59 +0530

    Update JobList1.jsx

--- JobList1.jsx
+++ JobList1.jsx
@@ -1,33 +1,54 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import Search from "./Search";
 import JobItem from "./JobItem";
 
 function JobList1(jobs) {
+  const data2 = jobs.job;
   const [selectedFilters, setSelectedFilters] = useState([]);
+  const [filteredJobs, setFilteredJobs] = useState(data2);
 
   const buttonHandler = (e) => {
-    // const value1 = e.target.value;
-    // console.log(value1);
-    // setVal(value1);
-    // <Filter value={val} skillset={skills} />;
-
-    // console.log(e);
-
-    if (selectedFilters?.includes(e)) {
+    if (selectedFilters.includes(e)) {
     } else {
       setSelectedFilters((prev) => {
         return [...prev, e];
       });
     }
   };
+  const handleOnClick = (item) => {
+    setSelectedFilters((prev) => prev.filter((filter) => filter !== item));
+  };
 
-  console.log({ selectedFilters });
-  const data2 = jobs.job;
-  const list2 = data2.map((item, index) => {
-    // console.log(item);
-    return <JobItem key={index} job={item} buttonHandler={buttonHandler} />;
-  });
+  const clearHandleAll = () => {
+    setFilteredJobs(data2);
+    setSelectedFilters([]);
+  };
 
-  return <div className="job-list">{list2}</div>;
+  useEffect(() => {
+    if (selectedFilters.length === 0) {
+      setFilteredJobs(data2);
+    } else {
+      const filtered = data2.filter((job) =>
+        selectedFilters.every((filter) => job.skills.includes(filter))
+      );
+      setFilteredJobs(filtered);
+    }
+  }, [selectedFilters, data2]);
+
+  const list2 = () => {
+    return filteredJobs.map((item, index) => (
+      <>
+        <JobItem key={index} job={item} buttonHandler={buttonHandler} />
+        <Search
+          selectedFilters={selectedFilters}
+          handleOnClick={handleOnClick}
+          clearHandleAll={clearHandleAll}
+        />
+      </>
+    ));
+  };
+
+  return <div className="job-list">{list2()}</div>;
 }
 
 export default JobList1;
