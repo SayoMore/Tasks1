b933c41074ac28e84f88c0a975070839cd1073de
Sayali More <96719695+SayoMore@users.noreply.github.com>
2024-06-24 14:10:13 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Banner from "./Banner";
 import Search from "./Search";
 import JobList1 from "./JobList1";
@@ -6,22 +6,6 @@ import { data } from "./Data";
 import "../styles/App.css";
 
 function App() {
-  // const [searchQuery, setSearchQuery] = useState([]);
-
-  // useEffect(() => {
-  //   fetch(
-  //     "https://boppotech-admin.github.io/react-task-json.github.io/joblist.json"
-  //   )
-  //     .then((response) => {
-  //       if (!response.ok) {
-  //         throw new Error("Network response was not ok");
-  //       }
-  //       return response.json();
-  //     })
-  //     .then((data) => setJobs(data))
-  //     .catch((error) => console.error("Fetch error: ", error));
-  // }, []);
-
   return (
     <div className="App">
       <Banner />
@@ -32,4 +16,3 @@ function App() {
 }
 
 export default App;
-
