67541dc1a9515cc8bdfd22071be2459774463c0b
Sayali More <96719695+SayoMore@users.noreply.github.com>
2024-06-24 14:10:51 +0530

    Update Search.jsx

--- Search.jsx
+++ Search.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import "../styles/SearchBar.css";
 
-function SearchBar() {
+const Search = ({ selectedFilters = [], handleOnClick, clearHandleAll }) => {
+  const updatedList = selectedFilters.map((item, index) => {
+    return (
+      <>
+        <button
+          className="searchbtn"
+          key={index}
+          onClick={() => handleOnClick(item)}
+        >{`${item} ❌`}</button>
+      </>
+    );
+  });
+
   return (
-    <div className="search-bar">
-      <input type="text"></input>
+    <div className="search-bar  flex3">
+      <div>{updatedList}</div>
+      <button className="clear" onClick={() => clearHandleAll()}>
+        {"Clear All"}
+      </button>
     </div>
   );
-}
-
-export default SearchBar;
+};
+export default Search;
